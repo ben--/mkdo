fddf54fead263873a6581fe69c68feb66991bf76
Ben Rogers <benrogers@mac.com>
2016-03-04 07:50:24 -0500

    Initial setup script

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+setup(
+    name='mkdo',
+    version='0.1.0',
+
+    description='Make Do - Manage Docker wrappers for build scripts',
+    long_description=long_description,
+
+    url='https://github.com/ben--/mkdo',
+    author='Ben Rogers',
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='docker build',
+
+    packages=['mkdo'],
+
+    data_files = {
+        'scripts': 'scripts/setup.sh',
+    },
+
+    entry_points = {
+        'console_scripts': [
+            'mkdo = mkdo:main',
+        ],
+    },
+)
