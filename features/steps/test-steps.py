f4ac4b12b9ee559d82610a4bb424f25b16d9e4d8
Ben Rogers <benrogers@mac.com>
2016-05-14 14:21:38 -0400

    First meaningful, failing acceptance test (stubbed steps)

---
+++ features/steps/test-steps.py
@@ -0,0 +1,13 @@
+from behave import given
+
+@given(u'a prompt in a source directory')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given a prompt in a source directory')
+
+@when(u'the user runs "mkdo create build"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When the user runs "mkdo create build"')
+
+@then(u'a "do/build" script is created')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then a "do/build" script is created')
--- features/test.feature
+++ features/test.feature
@@ -1,6 +1,6 @@
 Feature: Showing off behave
 
   Scenario: Run a simple test
-    Given we have behave installed
-     When we implement 5 tests
-     Then behave will test them for us!
+    Given a prompt in a source directory
+     When the user runs "mkdo create build"
+     Then a "do/build" script is created
